d6a617eff08f8777fa91467bb9f42bd44827d2f9
KomaSeo <ysseo0910@gmail.com>
2023-04-01 16:29:31 +0900

    fix ota

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -139,7 +139,7 @@ bool compare_thread_priorty(const struct list_elem *a, const struct list_elem *b
 int thread_get_priority_of(struct thread * m_thread);
 int thread_get_priority (void);
 void thread_set_priority (int);
-void thread_set_priorty_of(int new_priority, struct thread * m_thread);
+void thread_set_priority_of(int new_priority, struct thread * m_thread);
 
 int thread_get_nice (void);
 void thread_set_nice (int);
